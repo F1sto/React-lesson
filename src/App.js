e0754494ca65ddd5435aee54122b180e0dc5e706
User <anti-virus-13@mail.ru>
2020-07-26 18:05:40 +0600

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import Profile from './components/Profile';
+import Header from './components/Header.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper container'>
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='sidebar'>
+        <Sidebar />
+      </div>
+      <div className='content'>
+        <Profile />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return(<div>
+        <div>
+            <img className='img' src='https://bgfons.com/uploads/city/city_texture6420.jpg' />
+        </div>
+        <div>
+            ava + desk
+        </div>
+        <div>
+            posts
+            <div>
+                new psts
+            </div>
+            <div>
+                past 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>);
+}
+
+export default Profile;
